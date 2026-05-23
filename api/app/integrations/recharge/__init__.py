42b4d40d33e3a137029ebff5043a49dc0cc1b581
DavidSaatchyan <saatchy.d@gmail.com>
2026-05-23 18:14:35 +0800

    refactor: remove Recharge integration

--- api/app/integrations/recharge/__init__.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from .events import normalize_webhook
-from .actions import fetch_subscription_state
-
-__all__ = [
-    "normalize_webhook",
-    "fetch_subscription_state",
-]
